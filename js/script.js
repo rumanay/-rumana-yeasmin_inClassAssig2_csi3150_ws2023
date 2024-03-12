b6c6bcfb7efdd3ec7bc2b94b80dd2191d9a09e46
Rumana Yeasmin <ryeasmin@oakland.edu>
2024-03-12 11:01:46 -0400

    First commit

---
+++ js/script.js
@@ -0,0 +1,48 @@
+function buttonshowA() {
+  var x = document.getElementById("image-a");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function buttonshowB() {
+  var x = document.getElementById("image-b");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function buttonshowC() {
+  var x = document.getElementById("image-c");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function buttonhideA() {
+  var x = document.getElementById("image-a");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+function buttonhideB() {
+  var x = document.getElementById("image-b");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+function buttonhideC() {
+  var x = document.getElementById("image-c");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
